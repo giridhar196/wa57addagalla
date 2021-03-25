9527bb25ed1a707fb376a62b9a53f0f7560fc1f2
Giridhar Addagalla <giridhar196@gmail.com>
2021-03-24 20:33:56 -0500

    users

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var incValue = 0;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  incValue = incValue + 1;
   res.send('respond with a resource');
 });
 
